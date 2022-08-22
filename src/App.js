64db8749096fc9a2248af4f4bd125762fd3633a7
Mohammed Soharab S <soharab@quinta.co.in>
2022-08-23 00:52:25 +0530

    Memory update

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React, { useState } from 'react'
 import QrReader from 'react-qr-reader-es6';
 import useSound from 'use-sound';
 import beep from "./mp3/beep.mp3";
+import Button from '@mui/material/Button';
+
 function App() {
   const[result,setResult] = useState("Please scan first");
+  const[openScan,setOpenSCan] = useState(false);
   const handleError = (err)=> console.log(err);
+  
+  const openScanner = ()=>{
+    setOpenSCan(true);
+  }
   const handleScan =  (data)=>{
-    if(data) play();
+    if(data){
+      play();
+      setOpenSCan(false);
+    } 
     else  stop();
     setTimeout(1500);
     setResult(data);
   }
   const [play,{stop}] = useSound(beep);
   return (<>
-  
+           <Button onClick={openScanner} variant="contained">Scan</Button>
+            {
+            openScan && 
             <QrReader
                 delay={300}
                 onError={handleError}
                 onScan={handleScan}
                 style={{width:500,height: 500}}
-            />
+            />}
+            
         <p>{result}</p>
     </>
   )
